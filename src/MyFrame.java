430b5074e8bd70e02ebf925d88e0930e22bbeadb
AlihasanHasanov18101 <151041031+AlihasanHasanov18101@users.noreply.github.com>
2024-04-16 17:01:51 +0400

    branch commit

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -4,6 +4,8 @@ public class MyFrame extends JFrame {
     MyFrame(){
         this.setVisible(true);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setTitle("Crazy Kur");
+        this.setSize(900,900);
 
         ImageIcon image = new ImageIcon("src/image1.jpg");
         this.setIconImage(image.getImage());
